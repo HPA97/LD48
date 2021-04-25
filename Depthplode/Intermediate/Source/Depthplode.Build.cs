c58dc228a436ba1ba620b780ed8df26420fd2a8b
HPA97 <haakon568@gmail.com>
2021-04-25 21:00:17 +0200

    Init

---
+++ Depthplode/Intermediate/Source/Depthplode.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Depthplode : ModuleRules
+{
+	public Depthplode(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
